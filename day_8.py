473634df7bb45c4436adecd7e669f1d20f5dae6e
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-04-03 19:07:06 +0530

    Add files via upload

---
+++ day_8.py
@@ -0,0 +1,94 @@
+a=5
+# print('%d'%a)
+b=5.7
+# print('%d'%b)
+c=6
+# print('%f'%c)
+# print(float(c))
+d=6.7
+# print('%f'%d)
+# print('%.1f'%d)
+# print('%.2f'%d)
+# print('%.3f'%d)
+# print('%.4f'%d)
+# print('%3f'%d)
+# print('%4f'%d)
+# print('%8f'%d)
+# print('%9f'%d)
+# print('%10f'%d)
+
+a='py'
+# print('%s'%a)
+# print(str(a))
+# b=4
+# print(str(b))
+# print('%s'%b)
+# c=5.9
+# print(str(c))
+# print('%s'%c)
+
+a=5
+# print(a,type(a))
+# print("The Value of a is",a)
+# print("The Value of a is%d"%a)
+# print("The Value of a is%f"%a)
+# print("The Value of a is%s"%a)
+
+b=5.7
+# print(b,type(b))
+# print("The Valu of b is",b)
+# print("The Value of b is%d"%b)
+# print("The Value of b is%f"%b)
+# print("The Value of b is %s"%b)    
+
+c=2.47
+# print("The Value of c is%.2f"%c)
+# print("The Value of c is %s"%c)
+
+d=2
+print("The Value of of d is%s"%d)
+
+e=4.5+1.5
+# print('%f'%e)
+# print('%.1f'%e)
+# print('%.2f'%e)
+# print('%8f'%e)
+# print('%9f'%e)
+# print('%10f'%e)
+# print('%11f'%e)
+
+f=5.6+0.7
+g=0.5+0.7
+h=0.1+0.6
+# print(f)
+# print(g)
+# print(h)
+# print("The Value of h is%.1f"%h)
+# print("The Value of g is%.2f"%g)
+# print("The Value of f is%.3f"%f)
+
+i="Akhila"
+# print(i,type(i))
+# print("My Name is %s"%i)
+# print("My Name is%s"%i)
+# print("My Name is",i)
+
+# print('akhila'   ,'reddy')
+# print('akhila'+''+'reddy')
+# print('akhila'+' '+'reddy')
+# print('a','k','h','i','l','a',sep='$')
+
+print(ord('a'))
+print(ord('k'))
+print(ord('h'))
+print(ord('i'))
+print(ord('l'))
+print(ord('a'))
+
+print(chr(0))
+print(chr(65))
+print(chr(75))
+print(chr(72))
+print(chr(73))
+print(chr(76))
+print(chr(65))
